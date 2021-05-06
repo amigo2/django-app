452c94022a06fc0c2b5acbb80cb88d6e64045dd4
amigo2 <amigo2@hotmail.com>
2021-05-06 21:05:30 +0200

    try except

--- orders/views.py
+++ orders/views.py
@@ -16,8 +16,12 @@ class OrderViewSet(viewsets.GenericViewSet,
     serializer_class = serializers.OrderSerializer
 
     def get_queryset(self):
-        ''' return objects for the authenticate user only '''
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        try:
+            ''' return objects for the authenticate user only '''
+            return self.queryset.filter(user=self.request.user).order_by('-name')
+        except:
+            res = {"code": 404, "message": "Data not Found!"}
+            return Response(data=json.dumps(res), status=status.HTTP_404_NOT_FOUND)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -25,5 +29,5 @@ class OrderViewSet(viewsets.GenericViewSet,
     def get_serializer_class(self):
         if self.action == 'retrieve':
             return serializers.OrderDetailSerializer
-            
+
         return self.serializer_class
